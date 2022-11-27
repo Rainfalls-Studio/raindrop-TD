63d684d6bdf2d4e151d3bb6f3f31e2ea60ae11f8
Aalex <aaleex3984@gmail.com>
2022-11-27 17:35:23 +0100

    Initial commit

---
+++ src/layers/LayerStack.cpp
@@ -0,0 +1,31 @@
+#include "layers/LayerStack.hpp"
+
+namespace rnd::layers{
+	void LayerStack::init(){
+		PROFILE_FUNCTION();
+
+	}
+
+	void LayerStack::shutdown(){
+		PROFILE_FUNCTION();
+		clear();
+	}
+
+	void LayerStack::clear(){
+		PROFILE_FUNCTION();
+
+		for (auto &l : layers){
+			l.layer->~Layer();
+			memory::free(l.layer);
+		}
+		layers.clear();
+		nextID=0;
+	}
+
+	void LayerStack::update(const FrameData& data){
+		PROFILE_FUNCTION();
+		for (auto &l : layers){
+			l.layer->update(data);
+		}
+	}
+}
